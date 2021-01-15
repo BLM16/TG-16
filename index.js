0f4b7bff6dcffcc63fffef685c255a60d57d929e
Bradley Myers <myers.bradley@hotmail.com>
2021-01-14 20:26:06 -0500

    Bot now responds to pings instead of just prefix

--- index.js
+++ index.js
@@ -19,18 +19,26 @@ client.once('ready', () => {
     client.user.setStatus(presence.status);
     client.user.setActivity(presence.activity, { type: presence.activityType });
 
-    console.log(`Loaded ${client.commands.map(c => c).length} commands.`) 
+    console.log(`Loaded ${client.commands.map(c => c).length} commands and aliases.`) 
     console.log('TG-16 is online.')
 });
 
 client.on('message', msg => {
     if(msg.channel.type == 'dm') return;
 
-    if(msg.content.startsWith(prefix) && !msg.author.bot) { // Add ping reaction later
+    if(msg.content.startsWith(prefix) && !msg.author.bot) {
         const args = msg.content.slice(prefix.length).split(/ +/);
         const cmd = args.shift().toLowerCase();
 
         try { client.commands.get(cmd).execute(msg, args, client); }
+        catch(e) { return console.log(e); }
+    }
+
+    if(msg.content.startsWith(`<@!${client.user.id}>`) && !msg.author.bot) {
+        const args = msg.content.split(/ +/).slice(1);
+        const cmd = args.shift().toLowerCase();
+
+        try { client.commands.get(cmd).execute(msg, args, client) }
         catch(e) { return; }
     }
 });
